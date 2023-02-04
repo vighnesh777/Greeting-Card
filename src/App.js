93d334a4c50fb878145801c33161489809be1c03
Karicharla GNV Swamy Naidu <ganeshkaricherla@gmail.com>
2023-02-04 16:35:39 +0530

    Added CarouseL Plain In Utils

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
-
-import './App.css';
-import NavBar from './Components/NavBar/NavBar';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-=======
 import "./App.css";
-import { CarouselPlain } from "./Utils/Carousels/Carousels";
-
->>>>>>> 85e808e (Added CarouseL Plain In Utils)
+import NavBar from "./Components/NavBar/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/card' element={<Home/>}></Route>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/cards/:id' element={<Home/>}></Route>
-    </Routes>
-    <div className="App">
-<<<<<<< HEAD
-      <NavBar/>
-=======
-      <CarouselPlain
-        themeImage="https://picsum.photos/1200/300"
-        themeName="BIRTHDAY"
-      />
->>>>>>> 85e808e (Added CarouseL Plain In Utils)
-    </div>
+      <Routes>
+        <Route path="/card" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cards/:id" element={<Home />}></Route>
+      </Routes>
+      <div className="App">
+        <NavBar />
+      </div>
     </BrowserRouter>
   );
 }
